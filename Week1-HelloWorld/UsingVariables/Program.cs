ec10b9214dadae4e8a7bc1fd94940f0d91fa7571
VictorioMo <moraruvictorio@gmail.com>
2025-07-14 00:34:48 +0300

    Week1: Learning to use c# variables

---
+++ Week1-HelloWorld/UsingVariables/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace UsingVariables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            // declare a string type variable
+            string myFriendsName;
+
+            // assign value to the myFriendsName variable
+            myFriendsName = "Jonathan";
+
+            // use/access the variable
+            Console.WriteLine(myFriendsName);
+
+            // assign new value to the myFriendsName variable
+            myFriendsName = "Joseph";
+
+            // use/access the variable
+            Console.WriteLine(myFriendsName);
+
+            // declare a string type variable with initializer
+            string myFriendsName2 = "Jotaro";
+
+            Console.WriteLine($"My friend is {myFriendsName2}");
+
+            int myNumber = 10;
+            Console.WriteLine(myNumber);
+
+            float anotherNumber_f = 14.2f;
+            Console.WriteLine(anotherNumber_f);
+        }
+    }
+}
