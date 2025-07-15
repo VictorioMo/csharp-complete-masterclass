e5d3ee085ed2d3686290211d97ff8e41e28b1cdd
VictorioMo <moraruvictorio@gmail.com>
2025-07-15 22:27:56 +0300

    Section3 - Creating Section 3 along with 2 cool apps: GuessTheNumberGame and RocketLandingSimulator

---
+++ Section3-Loops/GuessTheNumberGame/Program.cs
@@ -0,0 +1,38 @@
+﻿int secretNumber = 0;  // The secret number which the user has to guess
+int userGuess    = 0;  // Number provided by user
+int tries        = 0;  // The number of tries the user made to guess the secret number
+
+Random random = new Random();
+
+secretNumber = random.Next(1, 101);
+
+Console.WriteLine("Guess the number between 1 and 100!");
+
+// The game should run until the user guesses the secret number
+while (userGuess != secretNumber)
+{
+    // Check if input is a number and ask the user to write a number in case of error
+    if (!(int.TryParse(Console.ReadLine(), out userGuess)))
+    {
+        Console.WriteLine("Please type a whole number.");
+    }
+    else
+    {
+        /* If the user input is greater or lower than the secret number
+        inform him so and increment the number of tries */
+        tries++;
+        if (userGuess > secretNumber)
+        {
+            Console.WriteLine("Too high!");
+        }
+        else if (userGuess < secretNumber)
+        {
+            Console.WriteLine("Too low!");
+        }
+        else
+        {
+            Console.WriteLine("You guessed! Congratulations!");
+            Console.WriteLine($"Number of tries: {tries}");
+        }
+    }
+}
---
+++ Section3-Loops/RocketLandingSimulator/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string rocket = "     |  \r\n     |  \r\n    /^\\ \r\n    | | \r\n    | | \r\n    | | \r\n    | | \r\n   ]|U|[\r\n    === ";
+
+string landingPad = "  _______";
+
+for (int counter = 0; counter < 9; counter++)
+{
+    landingPad = "\r\n" + landingPad;
+}
+
+for (int counter = 10; counter > 0; counter--)
+{
+    Console.Clear();
+    Console.WriteLine(rocket);
+    Console.WriteLine(landingPad);
+    landingPad = landingPad.Substring(2);
+    rocket = "\r\n" + rocket;
+    Thread.Sleep(1000);
+}
