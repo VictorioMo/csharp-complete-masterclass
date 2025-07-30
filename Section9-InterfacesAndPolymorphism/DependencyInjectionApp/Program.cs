ca5ddf1222d44cc6ee23d839cd15c8e33a14c5de
VictorioMo <moraruvictorio@gmail.com>
2025-07-30 21:20:26 +0300

    Section 9 - Section 9 completed with 2 new apps and readme file update.

---
+++ Section9-InterfacesAndPolymorphism/DependencyInjectionApp/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace DependencyInjectionApp
+{
+    public interface IToolUser
+    {
+        void SetHammer(Hammer hammer);
+        void SetSaw(Saw saw);
+    }
+
+    public class Hammer
+    {
+        public void Use()
+        {
+            Console.WriteLine("Hammering Nails!");
+        }
+    }
+
+    public class Saw
+    {
+        public void Use()
+        {
+            Console.WriteLine("Sawing wood!");
+        }
+    }
+
+    public class Builder : IToolUser /* Interface dependency injection */
+    {
+        private Hammer _hammer;
+        private Saw _saw;
+
+        /* Property dependency injection */
+        //public Hammer Hammer { get; set; }
+        //public Saw Saw { get; set; }
+
+        // Constructor dependency injection
+        //public Builder(Hammer hammer, Saw saw)
+        //{
+        //    _hammer = hammer;
+        //    _saw = saw;
+        //}
+
+        public void BuildHouse()
+        {
+            _saw.Use();
+            _hammer.Use();
+            Console.WriteLine("House built.");
+        }
+
+        public void SetHammer(Hammer hammer)
+        {
+            _hammer = hammer;
+        }
+
+        public void SetSaw(Saw saw)
+        {
+            _saw = saw;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hammer hammer = new Hammer();
+            Saw saw = new Saw();
+
+            Builder builder = new Builder();/*(hammer, saw);*/
+            builder.SetHammer(hammer);
+            builder.SetSaw(saw);
+
+            builder.BuildHouse();
+            
+        }
+    }
+}
---
+++ Section9-InterfacesAndPolymorphism/MultipleInheritanceApp/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace MultipleInheritanceApp
+{
+    public interface IPrintable
+    {
+        void Print();
+    }
+
+    public interface IScannable
+    {
+        void Scan();
+    }
+
+    public class MultiFunctionPrinter : IPrintable, IScannable
+    {
+        public void Print()
+        {
+            Console.WriteLine("Printing document.");
+        }
+
+        public void Scan()
+        {
+            Console.WriteLine("Scanning document.");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MultiFunctionPrinter printer = new MultiFunctionPrinter();
+
+            printer.Scan();
+            printer.Print();
+
+        }
+    }
+}
