8d82207ef1d4116b8580f39ac0a6633667853c4e
VictorioMo <moraruvictorio@gmail.com>
2025-08-13 22:22:29 +0300

    Section 13: Finished section 13 with 2 new apps to understand how to use WPF with ListBox and User Control & passwords.

---
+++ Section_13-WPF_WindowsPresentationFoundation/LoginFunctionalityApp/LoginView.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LoginFunctionalityApp
+{
+    /// <summary>
+    /// Interaction logic for LoginView.xaml
+    /// </summary>
+    public partial class LoginView : UserControl
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        private void OnLoginButtonClick(object sender, RoutedEventArgs e)
+        {
+            string enteredPassword = PasswordBox.Password;
+
+            string? envPw = Environment.GetEnvironmentVariable("TEST_LOGIN_APP_PASSWORD");
+
+            if (envPw != null)
+            {
+                if (enteredPassword == envPw)
+                {
+                    MessageBox.Show("Entered correct password.");
+                }
+                else
+                {
+                    MessageBox.Show("Entered wrong password.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Environment variable not found.");
+            }
+        }
+
+        private void OnPasswordChange(object sender, RoutedEventArgs e)
+        {
+            LoginButton.IsEnabled = !string.IsNullOrEmpty(PasswordBox.Password);
+        }
+    }
+}
