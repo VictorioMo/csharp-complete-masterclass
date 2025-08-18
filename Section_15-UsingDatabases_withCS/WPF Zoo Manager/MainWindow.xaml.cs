b79bb8e617d413724e54d9e1339b2c4774126831
VictorioMo <moraruvictorio@gmail.com>
2025-08-18 15:50:48 +0300

    Section 15: Zoo app (with sql)

---
+++ Section_15-UsingDatabases_withCS/WPF Zoo Manager/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Zoo_Manager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string baseConnectionString = ConfigurationManager.ConnectionStrings["WPF_Zoo_Manager.Properties.Settings.CSharpMasterClassConnectionString"].ConnectionString;
+            string connectionPassword = Environment.GetEnvironmentVariable("TEST_LOGIN_APP_PASSWORD");
+
+        }
+    }
+}
