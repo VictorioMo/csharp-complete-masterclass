b2a284ba4573087aebde58dceaffab16642b582f
VictorioMo <moraruvictorio@gmail.com>
2025-09-22 18:00:00 +0300

    Section 19: Section finished with 2 cool exercises.

---
+++ Section_19-ExercisesForCodingInterviews/HungryCats/Program.cs
@@ -0,0 +1,75 @@
+﻿/* === Exercise description === */
+/*
+ * [..] imagine you are in a kitchen that is full of cats. Every typical hungry cat will follow you if you hold some food, right?
+ * Our goal is to count not hungry cats in the kitchen.
+ * You with food in the kitchen will be marked as F
+ * Every cat will be represented as ~O or O~ depending on the direction.
+ * 
+ * Examples:
+ * 
+ * Input: "~O~O~O~O F"
+ * Return: 0(all cats follow you)
+ * 
+ * Input: "O~~O~O~O F O~O~"
+ * Return: 1(one is not following)
+ * [..]
+ */
+using System.Text.RegularExpressions;
+
+namespace HungryCats
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NotHungryCats("~O~O~O~O F"     ));
+            Console.WriteLine(NotHungryCats("O~~O~O~O F O~O~"));
+            Console.WriteLine(NotHungryCats("F ~OO~~O~O"     ));
+            Console.WriteLine(NotHungryCats("F O~O~~O O~"    ));
+        }
+
+        public static int NotHungryCats(string kitchen)
+        {
+            // The number of cats which are not hungry (return value)
+            int numberOfNotHungryCats = 0;
+
+            // Regex patterns to identify the cats inside the string (dependent on orientation side)
+            Regex findNotHungryCat_LeftSide  = new Regex("(O~)$+");
+            Regex findNotHungryCat_RightSide = new Regex("(~O)$+");
+
+            // Strings are separated in left path and right path
+            string[] sidePaths = kitchen.Split("F", 2);
+
+            // All spaces removed so only the cats are inside the strings (O~ or ~O)
+            string sidePathLeft  = sidePaths[0].Trim().Replace(" ", String.Empty);
+            string sidePathRight = sidePaths[1].Trim().Replace(" ", String.Empty);
+
+            // For each cat at the end of the string, identify if it's hungry or not
+            // Nevertheless, remove it from the string and repeat
+            while (sidePathLeft != String.Empty)
+            {
+                // Left side path of the string here
+                if (findNotHungryCat_LeftSide.IsMatch(sidePathLeft))
+                {
+                    numberOfNotHungryCats++;
+                }
+
+                sidePathLeft = sidePathLeft.Remove(sidePathLeft.Length - 2);
+            }
+
+
+            while (sidePathRight != String.Empty)
+            {
+                // Right side path of the string here
+                if (findNotHungryCat_RightSide.IsMatch(sidePathRight))
+                {
+                    numberOfNotHungryCats++;
+                }
+
+                sidePathRight = sidePathRight.Remove(sidePathRight.Length - 2);
+            }
+
+            return numberOfNotHungryCats;
+        }
+    }
+}
---
+++ Section_19-ExercisesForCodingInterviews/SumOfTwo/Program.cs
@@ -0,0 +1,44 @@
+﻿/* === Exercise description === */
+/*
+ * [..] write a method that will take two arguments: 
+ * a list of integers nums and an integer SumToFind. 
+ * And it returns the number of possible UNIQUE pares made from this list where the sum equals to SumToFind
+ * 
+ * Example:
+ * 
+ * SumOfTwo([1, 1, 1, 2, 3, 4, 5, 2], 2)
+ * 
+ * It should return: 1
+ * 
+ * Explanation: there is only one pair sum of witch is equal to 2 (1,1)
+ * ATTENTION: The main trick of this exercise is that its time complexity should be linear. 
+ * That means you should NOT have any double/triple loops inside or deep recursion.
+ */
+namespace SumOfTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPairs = SumOfTwo([1, 1, 1, 2, 3, 4, 5, 2], 2);
+
+            Console.WriteLine(numberOfPairs);
+        }
+
+        public static int SumOfTwo(int[] nums, int SumToFind)
+        {
+            int numberOfPairs = 0;
+
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                if (nums[i] + nums[i + 1] == SumToFind)
+                {
+                    numberOfPairs++;
+                    i++;
+                }
+            }
+
+            return numberOfPairs;
+        }
+    }
+}
